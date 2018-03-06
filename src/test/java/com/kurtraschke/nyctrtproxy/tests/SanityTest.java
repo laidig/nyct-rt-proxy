c70c39ee5511513e6f14d348b6a92a0d62285115
Simon Jacobs <sjacobs@camsys.com>
2018-03-06 16:52:11 -0500

    fix test - had duplicates but now we unmatch dups

--- src/test/java/com/kurtraschke/nyctrtproxy/tests/SanityTest.java
+++ src/test/java/com/kurtraschke/nyctrtproxy/tests/SanityTest.java
@@ -85,7 +85,7 @@ public class SanityTest extends RtTestRunner {
   // Test 5X -> 5 rewriting
   @Test
   public void test1_peak() throws Exception {
-    test(1, "1_peak_sample.pb", 280, 8);
+    test(1, "1_peak_sample.pb", 268, 20);
   }
 
   private void test(int feedId, String protobuf, int nScheduledExpected, int nAddedExpected) throws Exception {
