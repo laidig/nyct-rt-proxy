af0a4329b325714ef8bb712aff468a2ce72614ce
Tony Laidig <tony@Tonys-MacBook-Air.local>
2018-04-26 16:35:09 -0400

    better composed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget http://web.mta.info/developers/data/nyct/subway/google_transit.zip
-java -jar nyct-rt-proxy-1.0.7-SNAPSHOT-withAllDependencies.jar --config config
+java $JAVA_OPTIONS -server -jar nyct-rt-proxy-1.0.7-SNAPSHOT-withAllDependencies.jar --config config
 
